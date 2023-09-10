3c4ae8c46af332bcdc41df6f616fa3bf03eafbfe
Bruno Lorenzo <bruno.lorenzop@gmail.com>
2023-09-10 19:06:23 -0300

    Initial Commit

---
+++ demo-sfsymbols/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  demo-sfsymbols
+//
+//  Created by Bruno Lorenzo on 10/9/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ demo-sfsymbols/demo_sfsymbolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  demo_sfsymbolsApp.swift
+//  demo-sfsymbols
+//
+//  Created by Bruno Lorenzo on 10/9/23.
+//
+
+import SwiftUI
+
+@main
+struct demo_sfsymbolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
